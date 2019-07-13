7f9d00e08de952c1748be79d36a7b98436ce83c2
boob <huangbo@yy.com>
2019-07-13 23:19:29 +0800

    update gitbook

--- gitbook/gitbook/gitbook-plugin-baiduana/baidu_gitbook.js
+++ gitbook/gitbook/gitbook-plugin-baiduana/baidu_gitbook.js
@@ -9,4 +9,12 @@ require(["gitbook", "jQuery"], function(gitbook, $) {
         _hmt.push(['_trackEvent', location.pathname, 'page.change', '', '']);
     });
 
+    var _hmt = _hmt || [];
+    (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?426f1e4464116c54060fd43355f14f53";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+    })();
+
 });
